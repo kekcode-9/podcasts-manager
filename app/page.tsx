590ce8aad2362e8a5bd90cc6876fe3d21b988ef8
kekcode-9 <rupamita.s97@gmail.com>
2024-10-10 05:43:46 +0530

    useSearchParams wrapped in Suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import HomePage from "@/page-components/home"
+import { Suspense } from "react"
 
 export default function Home() {
   return (
-    <>
+    <Suspense>
       <HomePage/>
-    </>
+    </Suspense>
   )
 }
